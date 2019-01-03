36c93b86f39643a8fe96ff3880335707b2079015
happenzh <happenzh@gmail.com>
2019-01-03 17:29:40 +0800

    add bootstrap

--- yzh/src/app/app.module.ts
+++ yzh/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
